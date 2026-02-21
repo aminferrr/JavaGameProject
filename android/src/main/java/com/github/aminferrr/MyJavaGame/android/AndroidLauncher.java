95245cd2fc1edf3664087796433a37df2afd800d
aminferrr <panchenkoamina@gmail.com>
2026-02-22 02:43:27 +0500

    Initial LibGDX project

---
+++ android/src/main/java/com/github/aminferrr/MyJavaGame/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.github.aminferrr.MyJavaGame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.aminferrr.MyJavaGame.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
